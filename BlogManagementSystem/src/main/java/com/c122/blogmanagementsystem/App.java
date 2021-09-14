c25f5561dc56255b9f9820ae16678891d0eda95a
Joanne Pham <phamjoan@sheridancollege.ca>
2021-09-14 15:27:37 -0400

    starting project

---
+++ BlogManagementSystem/src/main/java/com/c122/blogmanagementsystem/App.java
@@ -0,0 +1,11 @@
+package com.c122.blogmanagementsystem;
+
+/**
+ *
+ * @author Joanne Pham
+ */
+public class App {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ BlogManagementSystem/src/main/java/com/c122/blogmanagementsystem/entity/Post.java
@@ -0,0 +1,9 @@
+package com.c122.blogmanagementsystem.entity;
+
+/**
+ *
+ * @author Joanne Pham
+ */
+public class Post {
+
+}
